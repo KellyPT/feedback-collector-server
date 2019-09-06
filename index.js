a1c136286c8e6a36f0eaa0cbf4654fb5f6630765
KellyPT <trankphuong@gmail.com>
2019-09-05 19:19:46 -0700

    changed greetings

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // single Express app here. we will use this to set up config
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
+    res.send({ hi: 'Kelly! How are you?'});
 });
 
 const PORT = process.env.PORT || 5000; // if there is no environment variable that already defined by Heroku, assign that variable to PORT. otherwise, use the default value of 5000.
